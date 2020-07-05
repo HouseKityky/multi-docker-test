ea55bc22148325e892d2ef318f36474c653bea9e
Yevhen Khabarov <yevhen.khabarov@uvoteam.com>
2020-07-05 15:50:34 +0300

    add redis setup

--- server/index.js
+++ server/index.js
@@ -18,7 +18,17 @@ const pgClient = new Pool({
     password: keys.pgPassword,
     port: keys.pgPort
 });
+
 pgClient.on('error', () => console.log('Lost PG connection'));
 pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch(err => console.log(err));
+
+// Redis Client Swtup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
